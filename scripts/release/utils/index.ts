e828dad353d1cec97ac6090a06588fea15c5342c
Jefferson Ribeiro <jefferson.ribeiro@mendix.com>
2022-10-19 17:56:32 +0200

    renamed release-utils-internal folder to utils

---
+++ scripts/release/utils/index.ts
@@ -0,0 +1,7 @@
+export * from "./version";
+export * from "./package-info";
+export * from "./shell";
+export * from "./git";
+export * from "./github";
+export * from "./config";
+export * from "./changelog-parser";
