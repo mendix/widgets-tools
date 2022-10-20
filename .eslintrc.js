aad3e4602988b89da7a6abc11545b202bc656567
Jefferson Ribeiro <jefferson.ribeiro@mendix.com>
2022-10-20 12:21:49 +0200

    fix eslint configuration for scripts

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,17 @@
-const deepmerge = require("deepmerge");
-
-const base = require("./packages/pluggable-widgets-tools/configs/eslint.ts.base.json");
-
-delete base.parserOptions.project;
-
-module.exports = deepmerge(base, {
+module.exports = {
+    ignorePatterns: ["scripts/release/utils/changelog-parser/parser/widget/widget.js"],
     rules: {
         "@typescript-eslint/ban-ts-ignore": "off",
         "no-unused-expressions": "off",
         "@typescript-eslint/no-unused-expressions": "error",
         "no-undef": "off"
-    }
-});
+    },
+    extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier", "plugin:prettier/recommended"],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        project: "./scripts/tsconfig.json",
+        ecmaVersion: 2018,
+        sourceType: "module"
+    },
+    plugins: ["@typescript-eslint"]
+};
