d47bb835fd78bdb17c24b4d53c87750169d5a6a2
Roman Vyakhirev <roman.vyakhirev@mendix.com>
2025-06-24 11:45:27 +0200

    Bump TS target of jest configs to es2019
    This target is supported by node 12+ and the tests are running in node environment only, so this is safe.

--- packages/pluggable-widgets-tools/test-config/jest.config.js
+++ packages/pluggable-widgets-tools/test-config/jest.config.js
@@ -13,7 +13,7 @@ module.exports = {
         "^.+\\.tsx?$": [
             "ts-jest",
             {
-                tsconfig: { module: "commonjs" }
+                tsconfig: { module: "commonjs", target: "ES2019" },
             }
         ],
         "^.+\\.jsx?$": join(__dirname, "transform.js"),
