60beb3a90baed2073dc67462437ac37d3d480be6
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2025-07-12 11:23:59 +0200

    refactor

--- packages/mpx/src/constants.ts
+++ packages/mpx/src/constants.ts
@@ -1,7 +1,14 @@
-import { readFileSync } from "node:fs"
+import { readFileSync } from "node:fs";
 
-const { version } = JSON.parse(
-    readFileSync(new URL("../package.json", import.meta.url)).toString()
-)
+const { version } = JSON.parse(readFileSync(new URL("../package.json", import.meta.url)).toString());
 
-export const VERSION = version as string
+export const VERSION = version as string;
+
+export const STD_EXTERNALS = [
+    // "mendix" and internals under "mendix/"
+    /^mendix($|\/)/,
+    /^react$/,
+    /^react\/jsx-runtime$/,
+    /^react-dom$/,
+    /^big.js$/
+];
