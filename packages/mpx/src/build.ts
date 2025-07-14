8a23f3ddda1b61eaa152ce65a1d0b9863e4c28f7
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2025-07-14 18:05:20 +0200

    chore: finish with mpk

--- packages/mpx/src/build.ts
+++ packages/mpx/src/build.ts
@@ -5,10 +5,12 @@ import ms from "pretty-ms";
 import { BuildOptions, build as buildBundle, watch } from "rolldown";
 import { onExit } from "signal-exit";
 import { STD_EXTERNALS } from "./constants.js";
-import { PackageJson } from "./lib/parsers/PackageJson.js";
 import { bold, green } from "./utils/colors.js";
 import { parsePackageError } from "./utils/error.js";
 import { logger } from "./utils/logger.js";
+import { createMPK } from "./utils/mpk.js";
+import { PackageJson } from "./utils/parsers/PackageJson.js";
+import { ProjectConfig } from "./utils/project-config.js";
 
 interface BuildCommandOptions {
     watch?: boolean;
@@ -40,7 +42,11 @@ export async function runBuild(root: string | undefined, options: BuildCommandOp
 
     const bundles = await loadConfig(project);
 
-    await fs.rm(project.outputDirs.dist, { recursive: true, force: true });
+    // await fs.rm(project.outputDirs.dist, { recursive: true, force: true });
+
+    console.dir(project.inputFiles);
+    console.dir(project.outputDirs);
+    console.dir(project.outputFiles);
 
     if (!options.watch) {
         buildMeasure.start();
@@ -48,6 +54,8 @@ export async function runBuild(root: string | undefined, options: BuildCommandOp
             await buildBundle(bundle);
             logger.success(pprintSuccessOutput(bundle.output?.file!));
         }
+        await createMPK(project.outputDirs.contentRoot, project.outputFiles.mpk);
+        logger.success(pprintSuccessOutput(project.outputFiles.mpk));
         buildMeasure.end();
     } else {
         logger.start("Start build in watch mode");
@@ -77,118 +85,9 @@ export async function runBuild(root: string | undefined, options: BuildCommandOp
     }
 }
 
-interface BundleInputFiles {
-    editorConfig: string;
-    editorPreview: string;
-    packageXml: string;
-    widgetFile: string;
-    widgetXml: string;
-}
-
-interface BundleOutputFiles {
-    editorConfig: string;
-    editorPreview: string;
-    esm: string;
-    umd: string;
-}
-
-interface BundleOutputDirs {
-    dist: string;
-    widgetDir: string;
-}
-
-interface ProjectConfigInputs {
-    pkg: PackageJson;
-    isTsProject: boolean;
-}
-
-class ProjectConfig {
-    readonly #dist = path.join("dist");
-    readonly #inputs: ProjectConfigInputs;
-    readonly pkg: PackageJson;
-    readonly isTsProject: boolean;
-    readonly configExt = "editorConfig";
-    readonly previewExt = "editorPreview";
-
-    constructor(inputs: ProjectConfigInputs) {
-        this.#inputs = inputs;
-        this.pkg = inputs.pkg;
-        this.isTsProject = inputs.isTsProject;
-    }
-
-    get files(): BundleInputFiles {
-        const { pkg, isTsProject } = this.#inputs;
-        const ext = isTsProject ? "ts" : "js";
-        const extJsx = isTsProject ? "tsx" : "jsx";
-
-        const editorConfig = path.format({
-            dir: "src",
-            name: pkg.widgetName,
-            ext: `${this.configExt}.${ext}`
-        });
-
-        const editorPreview = path.format({
-            dir: "src",
-            name: pkg.widgetName,
-            ext: `${this.previewExt}.${extJsx}`
-        });
-
-        const packageXml = path.format({
-            dir: "src",
-            base: "package.xml"
-        });
-
-        const widgetFile = path.format({
-            dir: "src",
-            name: pkg.widgetName,
-            ext: extJsx
-        });
-
-        const widgetXml = path.format({
-            dir: "src",
-            name: pkg.widgetName,
-            ext: "xml"
-        });
-
-        return { editorConfig, editorPreview, packageXml, widgetFile, widgetXml };
-    }
-
-    get outputDirs(): BundleOutputDirs {
-        const { pkg } = this.#inputs;
-        const widgetDir = path.join(this.#dist, ...pkg.packagePath.split("."), pkg.widgetName.toLowerCase());
-
-        return { dist: this.#dist, widgetDir };
-    }
-
-    get outputFiles(): BundleOutputFiles {
-        return {
-            esm: path.format({
-                dir: this.outputDirs.widgetDir,
-                name: this.pkg.widgetName,
-                ext: "mjs"
-            }),
-            umd: path.format({
-                dir: this.outputDirs.widgetDir,
-                name: this.pkg.widgetName,
-                ext: "js"
-            }),
-            editorConfig: path.format({
-                dir: this.outputDirs.dist,
-                name: this.pkg.widgetName,
-                ext: `${this.configExt}.js`
-            }),
-            editorPreview: path.format({
-                dir: this.outputDirs.dist,
-                name: this.pkg.widgetName,
-                ext: `${this.previewExt}.js`
-            })
-        };
-    }
-}
-
 async function defaultConfig(project: ProjectConfig): Promise<BuildOptions[]> {
     const esmBundle = {
-        input: project.files.widgetFile,
+        input: project.inputFiles.widgetFile,
         external: [...STD_EXTERNALS],
         output: {
             file: project.outputFiles.esm,
@@ -197,7 +96,7 @@ async function defaultConfig(project: ProjectConfig): Promise<BuildOptions[]> {
     } satisfies BuildOptions;
 
     const umdBundle = {
-        input: project.files.widgetFile,
+        input: project.inputFiles.widgetFile,
         external: [...STD_EXTERNALS],
         output: {
             file: project.outputFiles.umd,
@@ -210,7 +109,7 @@ async function defaultConfig(project: ProjectConfig): Promise<BuildOptions[]> {
     } satisfies BuildOptions;
 
     const editorConfigBundle = {
-        input: project.files.editorConfig,
+        input: project.inputFiles.editorConfig,
         output: {
             file: project.outputFiles.editorConfig,
             format: "commonjs"
@@ -218,7 +117,7 @@ async function defaultConfig(project: ProjectConfig): Promise<BuildOptions[]> {
     } satisfies BuildOptions;
 
     const editorPreviewBundle = {
-        input: project.files.editorPreview,
+        input: project.inputFiles.editorPreview,
         output: {
             file: project.outputFiles.editorPreview,
             format: "commonjs"
@@ -255,14 +154,14 @@ async function isTypeScriptProject(root: string): Promise<boolean> {
 }
 
 async function hasEditorConfig(project: ProjectConfig): Promise<boolean> {
-    return fs.access(path.resolve(project.files.editorConfig), fs.constants.F_OK).then(
+    return fs.access(path.resolve(project.inputFiles.editorConfig), fs.constants.F_OK).then(
         () => true,
         () => false
     );
 }
 
 async function hasEditorPreview(project: ProjectConfig): Promise<boolean> {
-    return fs.access(path.resolve(project.files.editorPreview), fs.constants.F_OK).then(
+    return fs.access(path.resolve(project.inputFiles.editorPreview), fs.constants.F_OK).then(
         () => true,
         () => false
     );
---
+++ packages/mpx/src/utils/project-config.ts
@@ -0,0 +1,129 @@
+import path from "node:path";
+import { PackageJson } from "./parsers/PackageJson.js";
+
+/** Files located in src directory */
+interface BundleInputFiles {
+    editorConfig: string;
+    editorPreview: string;
+    packageXml: string;
+    widgetFile: string;
+    widgetXml: string;
+}
+
+/** Files for that will be in final build output */
+interface BundleOutputFiles {
+    editorConfig: string;
+    editorPreview: string;
+    esm: string;
+    umd: string;
+    mpk: string;
+}
+
+interface BundleOutputDirs {
+    dist: string;
+    mpkDir: string;
+    contentRoot: string;
+    widgetDir: string;
+}
+
+interface ProjectConfigInputs {
+    pkg: PackageJson;
+    isTsProject: boolean;
+}
+
+export class ProjectConfig {
+    /** Output directory for built files */
+    readonly dist = "dist";
+    /** Package root directory that contains all widget files shipped with mpk */
+    readonly contentRoot = path.join(this.dist, "tmp", "widgets");
+    /** Widget package.json */
+    readonly pkg: PackageJson;
+    readonly isTsProject: boolean;
+
+    constructor(inputs: ProjectConfigInputs) {
+        this.pkg = inputs.pkg;
+        this.isTsProject = inputs.isTsProject;
+    }
+
+    get inputFiles(): BundleInputFiles {
+        const { pkg, isTsProject } = this;
+        const ext = isTsProject ? "ts" : "js";
+        const extJsx = isTsProject ? "tsx" : "jsx";
+
+        const editorConfig = path.format({
+            dir: "src",
+            name: pkg.widgetName,
+            ext: `editorConfig.${ext}`
+        });
+
+        const editorPreview = path.format({
+            dir: "src",
+            name: pkg.widgetName,
+            ext: `editorPreview.${extJsx}`
+        });
+
+        const packageXml = path.format({
+            dir: "src",
+            base: "package.xml"
+        });
+
+        const widgetFile = path.format({
+            dir: "src",
+            name: pkg.widgetName,
+            ext: extJsx
+        });
+
+        const widgetXml = path.format({
+            dir: "src",
+            name: pkg.widgetName,
+            ext: "xml"
+        });
+
+        return { editorConfig, editorPreview, packageXml, widgetFile, widgetXml };
+    }
+
+    /** Directory where widget bundles will be output */
+    get widgetDir(): string {
+        const { pkg, contentRoot } = this;
+        return path.join(contentRoot, ...pkg.packagePath.split("."), pkg.widgetName.toLowerCase());
+    }
+
+    get outputDirs(): BundleOutputDirs {
+        return {
+            dist: this.dist,
+            mpkDir: path.join(this.dist, this.pkg.version),
+            contentRoot: this.contentRoot,
+            widgetDir: this.widgetDir
+        };
+    }
+
+    get outputFiles(): BundleOutputFiles {
+        const { pkg, outputDirs } = this;
+        return {
+            esm: path.format({
+                dir: outputDirs.widgetDir,
+                name: pkg.widgetName,
+                ext: "mjs"
+            }),
+            umd: path.format({
+                dir: outputDirs.widgetDir,
+                name: pkg.widgetName,
+                ext: "js"
+            }),
+            editorConfig: path.format({
+                dir: outputDirs.contentRoot,
+                name: pkg.widgetName,
+                ext: `editorConfig.js`
+            }),
+            editorPreview: path.format({
+                dir: outputDirs.contentRoot,
+                name: pkg.widgetName,
+                ext: `editorPreview.js`
+            }),
+            mpk: path.format({
+                dir: outputDirs.mpkDir,
+                base: `${pkg.packagePath}.${pkg.widgetName}.mpk`
+            })
+        };
+    }
+}
