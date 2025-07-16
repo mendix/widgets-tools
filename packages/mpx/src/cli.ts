1516b36823f1f2d8fa5d92611094203179d7e326
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2025-07-16 12:53:50 +0200

    chore: change cli usage

--- packages/mpx/src/cli.ts
+++ packages/mpx/src/cli.ts
@@ -7,28 +7,23 @@ import { VERSION } from "./constants.js";
 
 const name = "mpx";
 const cli = cac(name);
+cli.usage(
+    `[options] [dir] \n\nBuild the widget in the specified directory. If the directory is omitted, use the current directory.`
+);
 
-cli.command("build [root]", "Build widget")
-    .option("-w, --watch", "watch for changes and rebuild", { default: false })
+cli.option("-w, --watch", "watch for changes and rebuild", { default: false })
     .option("-m, --minify", "minify the output (this option is 'on' in CI environment)", { default: Boolean(env.CI) })
-    .option("-p, --platform <platform>", "build platform (web or node)", { default: "web" })
-    .action(build);
+    .option("-p, --platform <platform>", "build platform (web or node)", { default: "web" });
 
 cli.help();
 cli.version(VERSION);
 
-cli.on("command:*", () => {
-    console.error(`Unknown command: "%s"`, cli.args.join(" "));
-    console.error(`See '${name} --help' for a list of available commands.`);
-    process.exit(1);
-});
+const {
+    args: [root],
+    options
+} = cli.parse();
 
-cli.parse();
-
-if (process.argv.length <= 2) {
-    cli.outputHelp();
-    process.exit(1);
-}
+build(root, options);
 
 process.on("uncaughtException", error => {
     console.error("Uncaught Exception:", error.message);
