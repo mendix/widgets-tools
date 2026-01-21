aaaca07d0da7ee359a790b7c2c38c89d66441048
Ali.Haydar.Sumer <ali.haydar.sumer@mendix.com>
2026-01-21 13:53:28 +0100

    Add unit tests

--- packages/pluggable-widgets-tools/src/typings-generator/__tests__/outputs/file.ts
+++ packages/pluggable-widgets-tools/src/typings-generator/__tests__/outputs/file.ts
@@ -3,7 +3,7 @@ export const fileWebOutput = `/**
  * WARNING: All changes made to this file will be overwritten
  * @author Mendix Widgets Framework Team
  */
-import { ActionValue, DynamicValue, EditableValue, FileValue } from "mendix";
+import { ActionValue, DynamicValue, EditableValue, EditableFileValue, FileValue } from "mendix";
 
 export interface MyWidgetContainerProps {
     name: string;
@@ -11,6 +11,7 @@ export interface MyWidgetContainerProps {
     id: string;
     file: DynamicValue<FileValue>;
     file2?: DynamicValue<FileValue>;
+    file3: EditableFileValue<FileValue>;
     description: EditableValue<string>;
     action?: ActionValue;
 }
@@ -21,6 +22,7 @@ export interface MyWidgetPreviewProps {
     translate: (text: string) => string;
     file: string;
     file2: string;
+    file3: string;
     description: string;
     action: {} | null;
 }
@@ -30,6 +32,7 @@ export const fileNativeOutput = `export interface MyWidgetProps<Style> {
     style: Style[];
     file: DynamicValue<FileValue>;
     file2?: DynamicValue<FileValue>;
+    file3: EditableFileValue<FileValue>;
     description: EditableValue<string>;
     action?: ActionValue;
 }`;
