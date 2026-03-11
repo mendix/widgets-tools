406f45f9b2013458971f931a03ac782b7d21c6b9
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2026-03-11 20:39:20 +0100

    chore: fix issue with build scripts

--- packages/pluggable-widgets-tools/src/index.ts
+++ packages/pluggable-widgets-tools/src/index.ts
@@ -1,10 +1,6 @@
 export * from "./common";
 export * from "./native/common";
 export * from "./utils";
-// Prevent tree shaking.
-export * from "./utils/enzyme-detector";
 export * from "./utils/typings";
-// Prevent tree shaking.
-export * from './utils/validation';
 export * from "./web/common";
 
--- packages/widget-typings-generator/build.mjs
+++ packages/widget-typings-generator/build.mjs
@@ -1,7 +1,6 @@
 import { build } from 'esbuild';
 import { execSync } from 'child_process';
 
-// Bundle JavaScript with esbuild
 await build({
   entryPoints: ['src/index.ts'],
   bundle: true,
@@ -10,13 +9,8 @@ await build({
   format: 'esm',
   outfile: 'dist/index.js',
   external: ['xml2js'],
-  sourcemap: true,
+  sourcemap: false,
   minify: false
 });
 
 console.log('✓ Bundled with esbuild');
-
-// Generate TypeScript declarations with tsc
-execSync('tsc --emitDeclarationOnly', { stdio: 'inherit' });
-
-console.log('✓ Generated TypeScript declarations');
